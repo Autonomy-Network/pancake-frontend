e44a3d63809e3a551d95ca7e0e7f4bc11a094730
daniel <kamhongro@gmail.com>
2021-10-01 10:31:54 -0700

    add log

--- src/components/autonomy/AutoHistory.tsx
+++ src/components/autonomy/AutoHistory.tsx
@@ -7,6 +7,7 @@ import AutoTransaction from './AutoTransaction'
 
 export default function AutoHistory({ type }) {
   const [transactions] = useTransactionHistory()
+  console.log(transactions)
   const allTokens = useAllTokens()
   const [currentTab, setCurrentTab] = useState('open')
   const mode = type
